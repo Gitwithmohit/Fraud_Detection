0f8340207d7f8cc6c3c1e39a1b4c7da8f1348244
gitwithmohit <badshh56@gmail.com>
2025-04-17 16:02:40 +0530

    model2 is the final model

--- ml_model.py
+++ ml_model.py
@@ -1,3 +1,14 @@
 
 
 print("Adding all ML related Code Here")
+
+def model1():
+    print("Print Model 1")
+
+def model2():
+    print("Print Model 2")
+
+def model3():
+    print("Print Model 3")
+
+print('model2 is performing best')
