204538529284df689cbefed80d2199bad0126be9
gitwithmohit <badshh56@gmail.com>
2025-04-17 14:49:20 +0530

    added a new function

---
+++ data_processing.py
@@ -0,0 +1,10 @@
+print("Adding all the data processing code related to fraud detection")
+
+
+a=5
+b=7
+def sum(a,b):
+    c=a+b
+    return a
+
+print("Added a new function Sum which is not correct")
